e5e4adef005c12cb9290d3a6239a329d900acc43
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-01-07 22:42:12 -0300

    Servindo HTML estático

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js" },
   resolve: { extensions: [".js", ".jsx"] },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
